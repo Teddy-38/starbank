69e96d202ad580b27dfd85f3b9e1a93e682fe62d
Teddy-38 <Zhdanov_as94@mail.ru>
2025-11-05 20:36:57 +0800

    Initial commit

---
+++ src/main/java/starbank/StarbankApplication.java
@@ -0,0 +1,13 @@
+package starbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/starbank/StarbankApplicationTests.java
@@ -0,0 +1,13 @@
+package starbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
